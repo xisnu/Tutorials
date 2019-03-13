71c44564de6ab75ec902bf5c77f551c5d7bb1793
Parthosarothi Mukherjee <parthosarothimukherjee@gmail.com>
2019-03-13 15:05:13 +0530

    Update UTHRNN_TF.py

--- UTHRNN_TF.py
+++ UTHRNN_TF.py
@@ -41,3 +41,20 @@ def UTHRNN(nodes,input,name,return_time_major=False,return_sequence=True):
     return output
     
     
+def FullyConnected(input,nbnodes,layername,give_prob=False):
+    shape=get_layer_shape(input)
+    in_dim=shape[-1]
+    # print("In dimesion ",in_dim)
+    dense_prob=None
+    W=tf.Variable(tf.truncated_normal([in_dim,nbnodes]),name=layername+"_W")
+    B=tf.constant(0.1,shape=[nbnodes],name=layername+"_B")
+    dense_out=tf.matmul(input,W)+B
+    if(give_prob):
+        dense_prob=tf.nn.softmax(dense_out)
+    return dense_out,dense_prob
+
+
+def get_layer_shape(layer):
+	thisshape = tf.Tensor.get_shape(layer)
+	ts = [thisshape[i].value for i in range(len(thisshape))]
+	return ts
